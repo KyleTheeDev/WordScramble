cf0babe7146528130c5ba549cc9a34fe87484239
KyleTheeDev <kandmiller@gmail.com>
2021-09-20 22:59:25 -0400

    Initial commit

---
+++ WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kyle Miller on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
